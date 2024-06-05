10387b71be9061102029e1355c0f99eecaedbe5c
charlyromytanga <charlyromytanga.cytech@gmail.com>
2024-06-05 21:22:53 +0200

    Toutes les pages

--- www/index.php
+++ www/index.php
@@ -1,19 +1,37 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>autooccas</title>
-    <link rel="stylesheet" href="">
-</head>
-<body>
-    
-</body>
-</html>
 
+    <head>
+        <meta charSet="utf-8" />
+        <title>autoccas.fr</title>
+        <link rel="stylesheet" href="../scr/css/bootstrap.min.css">
+        <link defer rel="stylesheet" href="../scr/javascript/bootstrap.bundle.min.js">
+    </head>
+
+    <body>
+        <header class="navbar navbar-expand-lg bd-navbar sticky-top">
+            <div class="container-xxl bd-gutter flex-wrap flex-lg-nowrap">
+                <ul class="navbar-nav flex-row flex-wrap bd-navbar-nav">
+                    <li class="nav-item col-6 col-lg-auto">Page une</li>
+                    <li class="nav-item col-6 col-lg-auto">Page deux</li>
+                </ul>
+                <ul class="navbar-nav flex-row flex-wrap bd-navbar-nav">
+                    <li class="nav-item col-6 col-lg-auto">recherche</li>
+                    <li class="nav-item col-6 col-lg-auto">infos</li>
+
+                </ul>
+            </div>
+        </header>
+
+        <footer>
+
+        </footer>
+    </body>
+
+</html>
 <?php
-    #phpinfo();
-    #àààààà fffgg
+
+
 ?>
 
 
