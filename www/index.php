82763a4dce0a7f1169aae3d23790f9c0b12e9f26
charlyromytanga <charlyromytanga.cytech@gmail.com>
2024-06-14 20:26:17 +0200

    Organistion

--- www/index.php
+++ www/index.php
@@ -11,7 +11,7 @@
 
     <header class="navbar navbar-expand navbar-dark flex-column flex-md-row bd-navbar">
         <a class="navbar-brand mr-0 mr-md-2" href="/" aria-label="Bootstrap">
-            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" class="d-block" viewBox="0 0 612 612" role="img" focusable="false"><title>Bootstrap</title><path fill="currentColor" d="M510 8a94.3 94.3 0 0 1 94 94v408a94.3 94.3 0 0 1-94 94H102a94.3 94.3 0 0 1-94-94V102a94.3 94.3 0 0 1 94-94h408m0-8H102C45.9 0 0 45.9 0 102v408c0 56.1 45.9 102 102 102h408c56.1 0 102-45.9 102-102V102C612 45.9 566.1 0 510 0z"/><path fill="currentColor" d="M196.77 471.5V154.43h124.15c54.27 0 91 31.64 91 79.1 0 33-24.17 63.72-54.71 69.21v1.76c43.07 5.49 70.75 35.82 70.75 78 0 55.81-40 89-107.45 89zm39.55-180.4h63.28c46.8 0 72.29-18.68 72.29-53 0-31.42-21.53-48.78-60-48.78h-75.57zm78.22 145.46c47.68 0 72.73-19.34 72.73-56s-25.93-55.37-76.46-55.37h-74.49v111.4z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" class="d-block" viewBox="0 0 612 612" role="img" focusable="false"><title>Autooccas</title><path fill="currentColor" d="M510 8a94.3 94.3 0 0 1 94 94v408a94.3 94.3 0 0 1-94 94H102a94.3 94.3 0 0 1-94-94V102a94.3 94.3 0 0 1 94-94h408m0-8H102C45.9 0 0 45.9 0 102v408c0 56.1 45.9 102 102 102h408c56.1 0 102-45.9 102-102V102C612 45.9 566.1 0 510 0z"/><path fill="currentColor" d="M196.77 471.5V154.43h124.15c54.27 0 91 31.64 91 79.1 0 33-24.17 63.72-54.71 69.21v1.76c43.07 5.49 70.75 35.82 70.75 78 0 55.81-40 89-107.45 89zm39.55-180.4h63.28c46.8 0 72.29-18.68 72.29-53 0-31.42-21.53-48.78-60-48.78h-75.57zm78.22 145.46c47.68 0 72.73-19.34 72.73-56s-25.93-55.37-76.46-55.37h-74.49v111.4z"/></svg>
         </a>
 
         <div class="navbar-nav-scroll">
@@ -20,16 +20,16 @@
                 <a class="nav-link" href="./index.php" onclick="ga('send', 'event', 'Navbar', 'Community links', 'Bootstrap');">Acceuil</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link active" href="/" onclick="ga('send', 'event', 'Navbar', 'Community links', 'Docs');">Acheter</a>
+                <a class="nav-link active" href="../www/scr/php/acheter.php" onclick="ga('send', 'event', 'Navbar', 'Community links', 'Docs');">Acheter</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="./vendre.php" onclick="ga('send', 'event', 'Navbar', 'Community links', 'Examples');">Vendre</a>
+                <a class="nav-link" href="../www/scr/php/vendre.php" onclick="ga('send', 'event', 'Navbar', 'Community links', 'Examples');">Vendre</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="./moncompte.php" onclick="ga('send', 'event', 'Navbar', 'Community links', 'Icons');" target="_blank" rel="noopener">Mon comptre</a>
+                <a class="nav-link" href="../www/scr/php/moncompte.php" onclick="ga('send', 'event', 'Navbar', 'Community links', 'Icons');" target="_blank" rel="noopener">Mon comptre</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="./FAQ.php" onclick="ga('send', 'event', 'Navbar', 'Community links', 'Themes');" target="_blank" rel="noopener">FAQ</a>
+                <a class="nav-link" href="../www/scr/php/FAQ.php" onclick="ga('send', 'event', 'Navbar', 'Community links', 'Themes');" target="_blank" rel="noopener">FAQ</a>
             </li>
             </ul>
         </div>
